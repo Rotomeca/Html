ddcb389e70c27a2d937e098d9e879c7b2c14d771
Rotomeca <delphin.tommy@gmail.com>
2023-10-17 17:11:56 +0200

    continuing

--- RotomecaHtml.js
+++ RotomecaHtml.js
@@ -3,17 +3,7 @@ class RotomecaHtml {
         this.balise = balise;
         this.attribs = attribs;
         this.childs = [];
-        this.parent = parent;
-        this.link = null;
-    }
-
-    _update_class() {
-        if (!this.attribs) this.attribs = {class:[]};
-        else if (!this.attribs.class) this.attribs.class = [];
-        else if (!!this.attribs.class && typeof this.attribs.class === 'string') this.attribs.class = this.attribs.class.split(' ');
-        else if (!!this.attribs.class && !this.attribs.class instanceof Array) this.attribs.class = [this.attribs.class];
-
-        return this;
+        this._parent = parent;
     }
 
     addClass(class_to_add) {
@@ -25,8 +15,32 @@ class RotomecaHtml {
         return this._update_class().attribs.class.includes(class_to_verify);
     }
 
+    css(key_or_attrib, value = '') {
+        if (typeof key_or_attrib === 'string') {
+            if (!this._update_css().attribs.style[key_or_attrib]) this.attribs.style[key_or_attrib] = value;
+        }
+        else {
+            for (const key in key_or_attrib) {
+                if (Object.hasOwnProperty.call(key_or_attrib, key)) {
+                    const element = key_or_attrib[key];
+                    this.css(key, element);
+                }
+            }
+        }
+
+        return this;
+    }
+
+    parent() {
+        return this._parent;
+    }
+
     tag(balise, attribs = {}) {
         return this._create(balise, this, attribs, false);
+    }
+
+    tag_one_line(balise, attribs = {}) {
+        return this._create_oneline(balise, this, attribs);
     }
 
     div(attribs = {}) {
@@ -37,16 +51,62 @@ class RotomecaHtml {
         return this.tag('span', attribs);
     }
 
+    img(src, attribs = {}){
+        attribs.src = src; 
+        return this.tag_one_line('img', attribs);
+    }
+
+    input(attribs = {}) {
+        return this.tag_one_line('input', attribs);
+    }
+
+    select(attribs = {}) {
+        return this.tag('select', attribs);
+    }
+
+    option(attribs = {}){
+        return this.tag('option', attribs);
+    }
+
+    option_one_line(value, text, attribs = {}){
+        attribs.value = value;
+        return this.option().text(text).end();
+    }
+
+    textarea(attribs = {}) {
+        return this.tag('textarea', attribs);
+    }
+
+    br(){
+        return this.tag_one_line('br');
+    }
+
+    comment(text) {
+        return this.text(`<!-- ${text} -->`);
+    }
+
+    _(commentary){
+        return this;
+    }
+
     text(text) {
         return this._create(text, this, {is_raw:true}, true);
     }
 
     end(debug = null) {
-        let end = this.parent._create(`/${this.balise}`, this.parent, null, true);
+        let end = this._parent._create(`/${this.balise}`, this._parent, null, true);
 
         if (!!debug) end.text(`<!-- ${debug} -->`);
 
         return end;
+    }
+
+    generate() {
+        return this._generate({mode:1});
+    }
+
+    generate_html({joli_html = false}) {
+        return this._generate({joli_html});
     }
 
     _create(balise, parent, attribs, isend) {
@@ -55,35 +115,58 @@ class RotomecaHtml {
         return isend ? parent : this.childs[this.childs.length - 1];
     }
 
-    generate() {
-        return this._generate({mode:1});
+    _create_oneline(balise, parent, attribs){
+        return this._create(balise, parent, attribs, true);
     }
 
-    generate_html() {
-        return this._generate({});
+    _update_class() {
+        if (!this.attribs) this.attribs = {class:[]};
+        else if (!this.attribs.class) this.attribs.class = [];
+        else if (!!this.attribs.class && typeof this.attribs.class === 'string') this.attribs.class = this.attribs.class.split(' ');
+        else if (!!this.attribs.class && !this.attribs.class instanceof Array) this.attribs.class = [this.attribs.class];
+
+        return this;
     }
 
+    _update_css() {
+        if (!this.attribs) this.attribs = {style:{}};
+        else if (!this.attribs.style) this.attribs.style = {};
 
+        return this;
+    }
 
     _generate({
         i = -1,
-        mode = 0
+        mode = 0,
+        joli_html = false
     }) {
         let html = [];
         
-        if ('start' !== this.balise) html.push(`${this._create_blanks(i)}${this._get_balise()}`);
+        if ('start' !== this.balise) html.push(`${this.balise !== '/textarea' && joli_html ? this._create_blanks(i) : ''}${this._get_balise()}`);
 
         for (const iterator of this.childs) {
-            html.push(iterator._generate({i:i + 1, mode}));
+            html.push(iterator._generate({i:i + 1, joli_html}));
         }
 
-        html = html.join('\r\n');
+        html = html.join(joli_html ? '\r\n' : '');
 
         switch (mode) {
             case 0:
                 break;
             case 1:
                 html = $(html);
+
+                for (const key in this.attribs) {
+                    if (Object.hasOwnProperty.call(this.attribs, key)) {
+                        if (key.includes('on'))
+                        {
+                            const element = this.attribs[key];
+
+                            if (typeof element === 'function') html.on(key.replace('on', ''), element);
+                        }
+                    }
+                }
+
                 break ;
             default:
                 throw new Error('mode not exist');
@@ -93,11 +176,12 @@ class RotomecaHtml {
     }
 
     _get_balise(){
+        const memory_tag = typeof this.balise === 'function' ? this.balise(this) : this.balise;
         let balise;
 
-        if (true === this.attribs?.is_raw) balise = this.balise;
+        if (true === this.attribs?.is_raw) balise = memory_tag;
         else {
-            balise = [`<${this.balise}`];
+            balise = [`<${memory_tag}`];
 
             if (typeof this.attribs === 'string') {
                 if ('' !== this.attribs) balise.push(this.attribs);
@@ -198,3 +282,21 @@ class RotomecaHtml {
     }
 }
 
+RotomecaHtml.ids = [];
+RotomecaHtml.generate_ids = function makeid(length = 5) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+
+    if (this.ids.includes(result)) result = RotomecaHtml.generate_ids(~~(Math.random()*100));
+
+    return result;
+}
+RotomecaHtml.remove_id = function (id) {
+    RotomecaHtml.ids = RotomecaHtml.ids.filter(x => x !== id);
+}
